eb810ebc05396ac5e5d875cc0b108340a21def09
domaro <diasomarov90@gmail.com>
2015-04-30 16:54:45 +0100

    auto search

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,10 +1,13 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
 	var self = this;
+	var token = "d5a9952caad7ee9d1d2ffeb96730363bdfacea8f";
 	var searchResource = $resource('https://api.github.com/search/users');
 
 	self.doSearch = function (){
 		self.searchResult = searchResource.get(
-			{ q: self.searchTerm }
+			{ q: self.searchTerm,
+				access_token: token
+			}
 		);
 	};
   
