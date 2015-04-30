5718fc9bcde00cfaac2a57ddbf4915eab1649c21
jaymuk <jaymuk.business@gmail.com>
2015-04-30 16:26:11 +0100

    more tests and passes

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -7,19 +7,28 @@ describe('GitUserSearchController', function() {
     ctrl = $controller('GitUserSearchController');
   }));
 
+  var httpBackend;
+
+  beforeEach(inject(function($httpBackend) {
+    httpBackend = $httpBackend;
+    httpBackend
+      .when("GET", "https://api.github.com/search/users?q=hello")
+      .respond(
+        { items: items }
+      );
+  }));
+
   it('initialises with an empty search result and term', function() {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
 
-  describe('when searching for a user',function() {
-
-  	var items = [
+    var items = [
       {
         "login": "tansaku",
         "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
         "html_url": "https://github.com/tansaku"
-      }, 
+      },
       {
         "login": "stephenlloyd",
         "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
@@ -27,9 +36,12 @@ describe('GitUserSearchController', function() {
       }
     ];
 
+  describe('when searching for a user',function() {
+
     it('displays search results', function() {
       ctrl.searchTerm = "hello";
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
 
