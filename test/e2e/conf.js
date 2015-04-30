e3b5e0a7c3c1dbbd9afad381f3cf9382395d695d
jaymuk <jaymuk.business@gmail.com>
2015-04-30 15:44:36 +0100

    created controller and accompanying unit test

--- test/e2e/conf.js
+++ test/e2e/conf.js
@@ -2,7 +2,8 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['githubProfileFeature.js'],
   multiCapabilities: [{
-  	browserName: 'firefox'
+  
+    browserName: 'firefox'
   }, {
     browserName: 'chrome'
   }]
